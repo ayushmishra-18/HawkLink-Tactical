1f76350a8df52ff745c443c52ffb33822734244c
ayushmishra-18 <2006ayushmishra@gmail.com>
2025-12-13 13:07:06 +0530

    Description of what you changed

--- soldiers_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ soldiers_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import battery_plus
+import flutter_tts
 import geolocator_apple
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BatteryPlusMacosPlugin.register(with: registry.registrar(forPlugin: "BatteryPlusMacosPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
