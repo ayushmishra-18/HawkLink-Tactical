dd8c272677b668bf3c2becae3a4b0c4487c482f4
ayushmishra-18 <2006ayushmishra@gmail.com>
2025-12-13 10:57:32 +0530

    Fix assets folder structure

--- commander_console/android/app/src/main/kotlin/com/example/commander_console/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.commander_console
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- soldiers_app/android/app/src/main/kotlin/com/example/hawklink_client/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hawklink_client
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
